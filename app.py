8d1e7fafc5a3a9ae74287d5d9780ed612645490a
UdhavPawar <upawar78@gmail.com>
2021-09-23 15:25:23 -0700

    🔱 core app

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ from datetime import datetime # for current date and time
 import pyautogui # to automate typing and mouse movements (pip3 install pyautogui)
 import pandas as pd # to load data from csv file (pip3 install pandas)
 # pip3 install Pillow : required for pyautogui to open, locate and click center of image
+import progressbar # for animated loading bar while searching for scheduled meetings
 
-# sign into zoom app
-def sign_into_zoom_app(input_zoom_meeting_id, input_zoom_meeting_pwd):
+# join zoom meeting
+def join_zoom_meeting(input_zoom_meeting_id, input_zoom_meeting_pwd):
     # edge case: error "This meeting ID is not valid. Please check and try again." occurs sometimes when zoom app was minimized (not quit) hence it can have meeting ID cached. So quitting app at start will clear it
     # figure out how to quit zoom app
 
@@ -32,7 +33,7 @@ def sign_into_zoom_app(input_zoom_meeting_id, input_zoom_meeting_pwd):
     join_buttton = pyautogui.locateCenterOnScreen("zoom_join_meeting_homescreen_button.png")
     pyautogui.moveTo(join_buttton)
     pyautogui.click()
-    print("Starting to join a new meeting\n")
+    print("Selecting join a new meeting\n")
     time.sleep(2)
 
     # insert zoom meeting id
@@ -56,7 +57,7 @@ def sign_into_zoom_app(input_zoom_meeting_id, input_zoom_meeting_pwd):
     join_buttton = pyautogui.locateCenterOnScreen("zoom_join_meeting.png")
     pyautogui.moveTo(join_buttton)
     pyautogui.click()
-    print("Joining meeting ID: {} \n".format(input_zoom_meeting_id))
+    print("Entered meeting ID: {} \n".format(input_zoom_meeting_id))
     time.sleep(1)
 
     # enter meeting password
@@ -66,21 +67,65 @@ def sign_into_zoom_app(input_zoom_meeting_id, input_zoom_meeting_pwd):
     time.sleep(0.5)
     pyautogui.press("enter") # enters the password
     print("Entered meeting password: {} \n".format(input_zoom_meeting_pwd))
-    print("# JOINED MEETING {} #\n".format(input_zoom_meeting_id))
+    # print("# JOINED MEETING {} #\n".format(input_zoom_meeting_id))
 
+    return "### JOINED MEETING {} ###\n".format(input_zoom_meeting_id)
+
+    """
     # select join with computer audio prompt on joining the meeting
     time.sleep(10) # edge case: if meeting is password protected then we don't know when host will admit us hence adding a default sleep time of 1 min before clicking on join with computer audio prompt
     select_computer_audio_button = pyautogui.locateCenterOnScreen("zoom_meeting_select_join_with_computer_audio_on_joining_meeting.png")
     pyautogui.moveTo(select_computer_audio_button)
     pyautogui.click()
 
-    """
+    
     # quit zoom meeting
     time.sleep(10)
     pyautogui.hotkey("ctrl", "w")
     pyautogui.press("enter")
     """
 
+
+# scheduler
+def meetings_scheduler():
+    # Read meetings csv file using pandas
+    data = pd.read_csv("meetings.csv")
+
+    # while loop which will keep checking for a meeting
+    already_joined_a_meeting = False
+
+    # animated loading bar
+    print("\nLooking for scheduled meetings: \n")
+    widgets = [progressbar.AnimatedMarker()]
+    bar, bar_counter = progressbar.ProgressBar(widgets=widgets).start(), 0
+
+    while not already_joined_a_meeting:
+        # checking if current time == meeting start time
+        now = datetime.now().strftime("%H:%M")
+        if now in  str(data["meeting_start_time"]):
+            # nagivate to that row using pandas loc in csv so other meeting details can be retrieved
+            data_row = data.loc[data["meeting_start_time"] == now]
+            # iloc is to iterate over a single row selected hence row will be 0
+            meeting_id = str(data_row.iloc[0,1]) # row 0 col 1
+            meeting_pwd = str(data_row.iloc[0,2]) # row 0 col 2
+            print("\n\n### Starting to join meeting: {}### \n".format(meeting_id))
+            print(join_zoom_meeting(meeting_id, meeting_pwd))
+            already_joined_a_meeting = True
+        else:
+            time.sleep(0.1)
+            if bar_counter > 3: # edge case: ValueError: Value out of range if we keep incrementing counter
+                bar_counter = 0
+                bar.start()
+            bar_counter += 1
+            bar.update(bar_counter)
+
 if __name__ == "__main__":
-    # sign_into_zoom_app("6507456272","cG5hYlN4dUg1Qk0xcEFMMHdJSk5ZQT09")
-    sign_into_zoom_app("9770788229","VGEyTjNKbGJUYzkxRHdSaDkwaTZhdz09")
+    meetings_scheduler()
+
+
+
+
+    """
+    join_zoom_meeting("6507456272","cG5hYlN4dUg1Qk0xcEFMMHdJSk5ZQT09")
+    # join_zoom_meeting("9770788229","VGEyTjNKbGJUYzkxRHdSaDkwaTZhdz09")
+    """
