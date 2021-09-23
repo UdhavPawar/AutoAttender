3a96c2ae7fe0a115dda7aeec56196ab98c4d1d61
UdhavPawar <upawar78@gmail.com>
2021-09-22 23:47:40 -0700

    🔱 core app

--- app.py
+++ app.py
@@ -17,28 +17,62 @@ import pandas as pd # to load data from csv file (pip3 install pandas)
 
 # sign into zoom app
 def sign_into_zoom_app(input_zoom_meeting_id, input_zoom_meeting_pwd):
+    # edge case: error "This meeting ID is not valid. Please check and try again." occurs sometimes when zoom app was minimized (not quit) hence it can have meeting ID cached. So quitting app at start will clear it
+    os.close
+
     # open zoom app
     subprocess.call(["/usr/bin/open", "/Applications/zoom.us.app"])
     print("Opened zoom app\n")
 
     # wait for zoom app to load completely
-    time.sleep(5)
-    print("Waited 5secs for zoom app to finish loading\n")
+    time.sleep(3)
+    print("Waited 3 secs for zoom app to finish loading\n")
 
-    # join zoom meeting
-    join_buttton = pyautogui.locateCenterOnScreen("zoom_meeting_join_button.png")
+    # select join a new zoom meeting option
+    join_buttton = pyautogui.locateCenterOnScreen("zoom_join_meeting_homescreen_button.png")
     pyautogui.moveTo(join_buttton)
     pyautogui.click()
-    print("Joining a new meeting\n")
+    print("Starting to join a new meeting\n")
     time.sleep(2)
 
     # insert zoom meeting id
     insert_meeting_id_button = pyautogui.locateCenterOnScreen("zoom_insert_meeting_id_button.png")
     pyautogui.moveTo(insert_meeting_id_button)
-    pyautogui.click()
+    pyautogui.doubleClick() # edge case: erase any previous meeting id. hence double click will select all text and in next line we simply write which overwrites the text
     pyautogui.write(input_zoom_meeting_id)
-    print("Entered meeting ID:", input_zoom_meeting_id, "\n")
-    time.sleep(2)
+    print("Entered meeting ID: {} \n".format(input_zoom_meeting_id))
+    time.sleep(1)
+
+    # disable both the camera and mic
+    # note: if camera / mic is already disabled (has a blue checkmark on app) it will simply ignore
+    media_button = pyautogui.locateAllOnScreen("zoom_disable_camera_and_mic_button.png")
+    for button in media_button:
+        pyautogui.moveTo(button)
+        pyautogui.click()
+        time.sleep(1)
+    print("Disabled meeting media options\n")
+
+    # join the zoom meeting
+    join_buttton = pyautogui.locateCenterOnScreen("zoom_join_meeting.png")
+    pyautogui.moveTo(join_buttton)
+    pyautogui.click()
+    print("Joining meeting ID: {} \n".format(input_zoom_meeting_id))
+    time.sleep(1)
+
+    # enter meeting password
+    insert_meeting_password_button = pyautogui.locateCenterOnScreen("zoom_insert_meeting_password_button.png")
+    pyautogui.moveTo(insert_meeting_password_button)
+    pyautogui.write(input_zoom_meeting_pwd)
+    time.sleep(0.5)
+    pyautogui.press("enter") # enters the password
+    print("Entered meeting password: {} \n # JOINED MEETING {} #\n".format(input_zoom_meeting_pwd, input_zoom_meeting_id))
+
+    # select join with computer audio prompt on joining the meeting
+    time.sleep(30) # edge case: if meeting is password protected then we don't know when host will admit us hence adding a default sleep time of 1 min before clicking on join with computer audio prompt
+    select_computer_audio_button = pyautogui.locateCenterOnScreen("zoom_meeting_select_join_with_computer_audio_on_joining_meeting.png")
+    pyautogui.moveTo(select_computer_audio_button)
+    pyautogui.click()
+
 
 if __name__ == "__main__":
-    sign_into_zoom_app("72200971764","L3p3Nm5OenBuZVNSRVczMHJGbVAvdz09")
+    sign_into_zoom_app("6507456272","cG5hYlN4dUg1Qk0xcEFMMHdJSk5ZQT09")
